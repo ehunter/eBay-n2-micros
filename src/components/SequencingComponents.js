19ce93cbfa7616a5be7b63d58725d5f28af7f20e
ehunter <erik.maxwell.hunter@gmail.com>
2018-09-04 15:29:36 -0500

    Add recommendation notes for sequencing

--- src/components/SequencingComponents.js
+++ src/components/SequencingComponents.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Badge from '../components/Badge';
+import PriceDrop from '../components/PriceDrop';
 import image1 from '../assets/images/laptop.jpg';
 import image2 from '../assets/images/sunglasses.jpg';
 import image3 from '../assets/images/nintendo.jpg';
@@ -22,19 +23,17 @@ class SequencingComponents extends Component {
             <div className="image-container"><img src={image1} /></div>
             <div className="item-tile-details-container">
               <h5><strong className="bold-text-2">苹果(Apple MacBook Air 11.6&quot; 笔记本电脑英特尔(Intel)</strong></h5>
-              <div>
-              <strong className="bold-text">¥2,865.71</strong></div>
+              <div><strong className="bold-text">¥2,865.71</strong></div>
             </div>
           </div>
           <div className="item-column w-col w-col-3 w-col-small-6 w-col-tiny-6">
             <div className="image-container"><img src={image2} /></div>
             <div className="item-tile-details-container">
               <h5><strong className="bold-text-2">Ray-Ban 中性镜像 Erika RB4171-601/55-54</strong></h5>
-              <div><strong className="bold-text">¥2,865.71</strong></div>
+              <PriceDrop initialPrice="￥1360" newPrice="￥1260" animDelay={500}></PriceDrop>
             </div>
           </div>
           <div className="item-column w-col w-col-3 w-col-small-6 w-col-tiny-6">
-            <Badge theme="discount" value="6.5折" animDelay={600}></Badge>
             <div className="image-container"><img src={image3} /></div>
             <div className="item-tile-details-container">
               <h5><strong className="bold-text-2">苹果(Apple) MacBook Air 11.6&quot; 笔记本电脑英特尔(Intel)</strong></h5>
@@ -42,6 +41,7 @@ class SequencingComponents extends Component {
             </div>
           </div>
           <div className="item-column w-col w-col-3 w-col-small-6 w-col-tiny-6">
+          <Badge theme="discount" value="6.5折" animDelay={1100}></Badge>
             <div className="image-container"><img src={image4} /></div>
             <div className="item-tile-details-container">
               <h5><strong className="bold-text-2">苹果(Apple) MacBook Air 11.6&quot; 笔记本电脑英特尔(Intel)</strong></h5>
@@ -58,7 +58,7 @@ class SequencingComponents extends Component {
             </div>
           </div>
           <div className="item-column w-col w-col-3 w-col-small-6 w-col-tiny-6">
-            <Badge theme="discount" value="4.5折" animDelay={850}></Badge>
+            <Badge theme="discount" value="4.5折" animDelay={1700}></Badge>
             <div className="image-container"><img src={image2} /></div>
             <div className="item-tile-details-container">
               <h5><strong className="bold-text-2">Ray-Ban 中性镜像 Erika RB4171-601/55-54</strong></h5>
@@ -73,7 +73,7 @@ class SequencingComponents extends Component {
             </div>
           </div>
           <div className="item-column w-col w-col-3 w-col-small-6 w-col-tiny-6">
-            <Badge theme="discount" value="6.5折" animDelay={1100}></Badge>
+            <Badge theme="discount" value="6.5折" animDelay={8000}></Badge>
             <div className="image-container"><img src={image4} /></div>
             <div className="item-tile-details-container">
               <h5><strong className="bold-text-2">苹果(Apple) MacBook Air 11.6&quot; 笔记本电脑英特尔(Intel)</strong></h5>
